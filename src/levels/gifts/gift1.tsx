4c9529ba85071cff7120d09d88e846b22de7f990
lumini23 <lumini2@hotmail.com.br>
2024-12-16 23:44:42 -0300

    fix: responsividade

--- src/levels/gifts/gift1.tsx
+++ src/levels/gifts/gift1.tsx
@@ -1,11 +1,8 @@
 import { BaseLevel } from "../../components/BaseLevel"
-import { useComment } from "../../hooks/useComment"
 
 function Gtank() {
   return (
-    <BaseLevel imagePath="moto.jpg">
-      {useComment('Qual é essa deusa?')}
-    </BaseLevel>   
+    <BaseLevel imagePath="moto.jpg"></BaseLevel>   
   )
 }
 
