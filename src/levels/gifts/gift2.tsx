8dff9d4901a3384dc257735280f23e0cfc8f1d33
andreversiani <lumini2@hotmail.com.br>
2025-08-07 03:18:18 -0300

    feat: add special day level

---
+++ src/levels/gifts/gift2.tsx
@@ -0,0 +1,9 @@
+import { BaseLevel } from "../../components/BaseLevel"
+
+function WeirdFishes() {
+  return (
+    <BaseLevel imagePath="casa.png"></BaseLevel>   
+  )
+}
+
+export default WeirdFishes
--- src/levels/gifts/routes.tsx
+++ src/levels/gifts/routes.tsx
@@ -1,8 +1,13 @@
 import Gtank from "./gift1";
+import WeirdFishes from "./gift2";
 
 export const giftsRoutes = [
     {
         path: '/special/gift',
         element: <Gtank />,
+    },
+    {
+        path: '/special/day',
+        element: <WeirdFishes />,
     },
 ]
